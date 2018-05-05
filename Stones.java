9f2bca2d53d82f61cc907b79f68369c15ddd697c
hieptran1208 <36745623+hieptran1208@users.noreply.github.com>
2018-05-05 02:43:59 -0700

    Update Stones.java

--- Stones.java
+++ Stones.java
@@ -2,8 +2,6 @@ package sample;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.event.ActionListener;
-import java.awt.geom.Ellipse2D;
 
 public class Stones implements MancalaDesign
 {
@@ -11,7 +9,8 @@ public class Stones implements MancalaDesign
 	private int y;
 	private int h;
 	private int w;
-	public Stones(int x, int y, int height, int width)
+	private int [][] stones;
+	public Stones(int x, int y, int height, int width, int [][] stone)
 	{
 		/* 
 		 * x=20, y=60, h=300, w=100
@@ -20,40 +19,39 @@ public class Stones implements MancalaDesign
 		this.y = y;
 		h = height;
 		w = width;
+		stones = stone;
 	}
 
 	// add an extra parameter type int[][] mancala to determine the number of each pit
 	public void draw(Graphics2D g2)
 	{
 		g2.setColor(Color.BLACK);
-		g2.drawString("10", x+w/2, y+h/2);
+		g2.drawString(stones[0][0] + "", x+w/2, y+h/2);
 		
-		g2.drawString("10", x + w + x+w/2, y +w/2);
+		g2.drawString(stones[0][1] + "", x + w + x+w/2, y +w/2);
 		
-		g2.drawString("10", x + 2*w + 2*x+w/2, y+w/2);
+		g2.drawString(stones[0][2] + "", x + 2*w + 2*x+w/2, y+w/2);
 		
-		g2.drawString("10", x + 3*w + 3*x+w/2, y+w/2);
+		g2.drawString(stones[0][3] + "", x + 3*w + 3*x+w/2, y+w/2);
 		
-		g2.drawString("10", x + 4*w + 4*x+w/2, y+w/2);
+		g2.drawString(stones[0][4] + "", x + 4*w + 4*x+w/2, y+w/2);
 		
-		g2.drawString("10", x + 5*w + 5*x+w/2, y+w/2);
+		g2.drawString(stones[0][5] + "", x + 5*w + 5*x+w/2, y+w/2);
 		
-		g2.drawString("10", x + 6*w + 6*x+w/2, y+w/2);
+		g2.drawString(stones[0][6] + "", x + 6*w + 6*x+w/2, y+w/2);
 
-		g2.drawString("10", x + 7*w + 7*x+w/2, y+h/2);
+		g2.drawString(stones[1][0] + "", x + 7*w + 7*x+w/2, y+h/2);
 		
-		g2.drawString("10", x + w + x+w/2, 4*y+w/2);
+		g2.drawString(stones[1][1] + "", x + w + x+w/2, 4*y+w/2);
 
-		g2.drawString("10", x + 2*w + 2*x+w/2, 4*y+w/2);
+		g2.drawString(stones[1][2] + "", x + 2*w + 2*x+w/2, 4*y+w/2);
 		
-		g2.drawString("10", x + 3*w + 3*x+w/2, 4*y+w/2);
+		g2.drawString(stones[1][3] + "", x + 3*w + 3*x+w/2, 4*y+w/2);
 		
-		g2.drawString("10", x + 4*w + 4*x+w/2, 4*y+w/2);
+		g2.drawString(stones[1][4] + "", x + 4*w + 4*x+w/2, 4*y+w/2);
 		
-		g2.drawString("10", x + 5*w + 5*x+w/2, 4*y+w/2);
+		g2.drawString(stones[1][5] + "", x + 5*w + 5*x+w/2, 4*y+w/2);
 		
-		g2.drawString("10", x + 6*w + 6*x+w/2, 4*y+w/2);
+		g2.drawString(stones[1][6] + "", x + 6*w + 6*x+w/2, 4*y+w/2);
 	}
-
-
 }
