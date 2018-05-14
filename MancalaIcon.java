27f0799ad5244eecbf4a39d246b79bbe62f0f7fc
Katrina <36499790+katrinatran@users.noreply.github.com>
2018-05-14 11:04:43 -0700

    FINAL
    Final version done with teammates

---
+++ MancalaIcon.java
@@ -0,0 +1,65 @@
+/**
+ * File name: MancalaIcon.java
+ * Class name: MancalaIcon
+ * Description: MancalaIcon class implements Icon.
+ * @author Katrina Tran
+ */
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.Icon;
+
+public class MancalaIcon implements Icon
+{
+	private MancalaDesign design;
+	private int height;
+	private int width;
+	/**
+	 * Constructor for MancalaIcon
+	 * @param style: MancalaDesign style that is chosen
+	 * @param w: width of icon
+	 * @param h : height of icon
+	 */
+	public MancalaIcon(MancalaDesign style, int w, int h)
+	{
+		design = style;
+		width = w;
+		height = h;
+	}
+	/**
+	 * gets the icon width
+	 * @return the icon width
+	 */
+	public int getIconWidth()
+	{
+		return width;
+	}
+	/**
+	 * gets the height of icon
+	 * @return returns icon height
+	 */
+	public int getIconHeight()
+	{
+		return height;
+	}
+	/**
+	 * gets design of icon
+	 * @return MancalaDesign of icon
+	 */
+	public MancalaDesign getDesign()
+	{
+		return design;
+	}
+	/**
+	 * draws the icon
+	 * @param c: component of icon
+	 * @param g: Graphics object of icon
+	 * @param x: x-corner of icon
+	 * @param y: y-corner of icon
+	 */
+	public void paintIcon(Component c, Graphics g, int x, int y)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		design.draw(g2);
+	}
+}
