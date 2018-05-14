dbf22f4b265a78353957ee6f188a62a6e81c3f32
Katrina <36499790+katrinatran@users.noreply.github.com>
2018-05-14 11:27:00 -0700

    FINAL
    Final done with teammates

---
+++ MancalaDesign.java
@@ -0,0 +1,16 @@
+/**
+ * File name: MancalaDesign.java
+ * Interface name: MancalaDesign
+ * Description: MancalaDesign has draw method.
+ * @author Katrina Tran
+ */
+import java.awt.Graphics2D;
+
+public interface MancalaDesign
+{
+	/**
+	 * draws stones and pits for each design of Mancala board.
+	 * @param g2: Graphics2D object
+	 */
+	void draw(Graphics2D g2);
+}
